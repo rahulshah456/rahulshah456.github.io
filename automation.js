8646bae3df5939deadb6a06085332408a6db4fed
Rahul <rahulkumarshah5000@gmail.com>
2020-12-28 00:00:38 +0530

    about page added

---
+++ automation.js
@@ -0,0 +1,12 @@
+var options = {
+    strings: [
+        'Hello World',
+        'I Turn <span class="highlight">Visions</span> Into Stunning Digital Experiences'
+    ],
+    typeSpeed: 50,
+    startDelay: 250,
+    showCursor: true,
+    cursorChar: '_'
+};
+
+var typed = new Typed('#introduction', options);
