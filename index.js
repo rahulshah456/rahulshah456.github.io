f1191b23c356108f167135d1c5ed873600b5d0ac
Rahul Shah <rahulkumarshah5000@gmail.com>
2021-08-23 16:11:38 +0530

    refactored stylesheets, fixed #2

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 
 //@ts-check
 
-// Desktop Navbar State Changer
-$("#navbar__desktop .nav-link").on("click", function(){
-	$("#navbar__desktop").find(".active").removeClass("active");
-	$(this).addClass("active");
+const navMobileMenus = document.querySelectorAll(".nav__mobile  .nav__link");
+
+navMobileMenus.forEach(navMobileMenu => {
+  navMobileMenu.addEventListener("click", event => {
+    navMobileMenus.forEach(menu => {
+      if(menu.classList.contains("nav__link--active")) {
+        menu.classList.remove("nav__link--active");
+      }
+    });
+    navMobileMenu.classList.add("nav__link--active");
+  });
 });
 
 
-// Mobile Navbar State Changer
-$(".nav__mobile .nav__link").on("click", function(){
-	$(".nav__mobile").find(".nav__link--active").removeClass("nav__link--active");
-	$(this).addClass("nav__link--active");
-});
-
-
-
-$('.exp__card').on('mouseenter', function(){
-	$(this).find('.exp__card-body').show(300, function() {
-		console.log('show');
-	});
-}).on('mouseleave', function(){
-	$(this).find('.exp__card-body').hide(300, function() {
-		console.log('hide');
-	});
-})
 
 
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
@@ -54,25 +44,35 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 
 
 
-let projectCards = document.querySelectorAll(".project__card");
+const projectCards = document.querySelectorAll(".project__card");
+var activeCard = -1;
 
-projectCards.forEach( cards => {
-	cards.addEventListener('mouseleave', () => {
-    console.log("mouseleave");
-		cards.getElementsByTagName('img')[0].style.filter = "blur(0px)";
-    cards.getElementsByClassName('project_card--info')[0].style.width = "0%";
-    if(cards.getElementsByClassName('project_card--desc')[0].style.display != "none") {
-      cards.getElementsByClassName('project_card--desc')[0].style.display = "none";
-    }
-	});
+projectCards.forEach( (cards,index) => {
+
 	cards.addEventListener('mouseenter', () => {
-    console.log("mouseenter");
-		cards.getElementsByTagName('img')[0].style.filter = "blur(8px)";
-    cards.getElementsByClassName('project_card--info')[0].style.width = "90%";
-    setTimeout(function() {
-      if(cards.getElementsByClassName('project_card--desc')[0].style.display != "flex") {
-        cards.getElementsByClassName('project_card--desc')[0].style.display = "flex";
-      }
-    },300);
+    
+    if(activeCard != index) {
+
+      //hide info view from any other active cards
+      projectCards.forEach( otherCard => {
+        otherCard.getElementsByTagName('img')[0].style.filter = "blur(0px)";
+        otherCard.getElementsByClassName('project_card--info')[0].style.width = "0%";
+        if(otherCard.getElementsByClassName('project_card--desc')[0].style.display != "none") {
+          otherCard.getElementsByClassName('project_card--desc')[0].style.display = "none";
+        }
+      })
+
+      //show the info view to current view
+      cards.getElementsByTagName('img')[0].style.filter = "blur(8px)";
+      const infoCard = cards.getElementsByClassName('project_card--info')[0];
+      infoCard.style.width = "90%";
+      infoCard.addEventListener('transitionend', () => {
+        if(cards.getElementsByClassName('project_card--desc')[0].style.display != "flex") {
+          cards.getElementsByClassName('project_card--desc')[0].style.display = "flex";
+        }
+      })
+
+      activeCard = index;
+    }
 	});
 });
